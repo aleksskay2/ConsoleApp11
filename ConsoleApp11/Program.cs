96dfff51a642bc65571ffe2618c8d5f6b7a4110f
aleksskay2 <aleksskay2@yandex.ru>
2021-04-04 13:25:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    abstract class First
+    {
+
+        public abstract int GetAge(); 
+    }
+
+    class Second:First
+    {
+        public Second() : base()
+        {
+
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            First first = new First();
+        }
+    }
+}
